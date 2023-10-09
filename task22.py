5eee7bbd7124c70fba19075e600f970226ef9cf7
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-10-09 23:33:53 +0300

    Задание есть

--- task22.py
+++ task22.py
@@ -1,17 +1,10 @@
-def newset(num):
-    new_set = set()
-    for i in range(num):
-        new_set.add(int(input("Введите число для множества: ")))
-    return new_set
-
-n = int(input("Введите кол-во элементов первого множества: "))
-n_set = newset(n)
-
-m = int(input("Введите кол-во элементов второго множества: "))
-m_set = newset(m)
-
-print(*n_set)
-print(*m_set)
-
-s_set = sorted(n_set.intersection(m_set))
-print(*s_set)
+def print_operation_table(operation, num_rows, num_columns):
+    if num_rows < 2 or num_columns < 2:
+        print('ОШИБКА! Размерности таблицы должны быть больше 2!')
+    else:
+        header = ' '.join([str(i) for i in range(1, num_columns + 1)])
+        print(header)
+        for i in range(2, num_rows + 1):
+            row = [str(i)] + [str(operation(i, j)) for j in range(2, num_columns + 1)]
+            print(' '.join(row))
+print_operation_table(lambda x, y: x * y, 3, 3)
